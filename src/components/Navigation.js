f828ea79e9fda31c41945901f8a733c408b5e510
Lucas <iesvda.lucrod@gmail.com>
2025-01-27 14:32:12 +0100

    TESTING  router WITH rBootstrap

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,25 +1,54 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
+
 import Test from '../pages/Test';
 import Test2 from '../pages/Test2';
+import ContactPage from '../pages/ContactPage';
 
 function Navigation() {
   return (
     <BrowserRouter>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">TEST!</Link>
-          </li>
-          <li>
-            <Link to="/about">TEST!</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    
+    <Container fluid>
+      <Row>
+        <Col>
+          <Navbar expand="md">
+            <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
+
+            {/*Need to fetch the categories for this  VVV Placeholder*/}
+            <Nav>
+              <Nav.Link as={Link} to="/test1">Test1</Nav.Link>
+              <Nav.Link as={Link} to="/test2">Test2</Nav.Link>
+            </Nav>
+                    {
+                      /*
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">
+                          Another action
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">
+                          Separated link
+                        </NavDropdown.Item>
+                        </NavDropdown>
+                      */
+                    }
+                      
+
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+
+
+          </Navbar>
+        </Col>
+      </Row>
+    </Container>
+
       <Routes>
-      <Route path="/" element={<Test />} />
-      <Route path="/about" element={<Test2 />} />
+        <Route path="/test1" element={<Test />} />
+        <Route path="/test2" element={<Test2 />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </BrowserRouter>
   );
